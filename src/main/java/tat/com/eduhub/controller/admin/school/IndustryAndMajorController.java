45f946aca30747eaa65b99f6f306847dde514692
tat <trieuanhtuan1408@gmail.com>
2024-04-02 02:20:24 +0700

    add controller industry_major

---
+++ src/main/java/tat/com/eduhub/controller/admin/school/IndustryAndMajorController.java
@@ -0,0 +1,21 @@
+package tat.com.eduhub.controller.admin.school;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import tat.com.eduhub.base.BASE_FIELD;
+import tat.com.eduhub.base.BASE_METHOD;
+
+@Controller
+@RequestMapping(value = "/s-admin/{domain}/nganh-chuyen-nganh")
+public class IndustryAndMajorController {
+
+	@GetMapping(value = {"","/them-moi"})
+	public String createIndustryOrMajorPage(Model model) {
+		BASE_METHOD.FragmentAdminSchool("create_industry_major", model);
+		return BASE_FIELD.SCHOOL_ADMIN_LAYOUT;
+	}
+	
+}
