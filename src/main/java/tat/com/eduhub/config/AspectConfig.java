221fe9d640f24eb9f4f6e9b82196430019daa4ae
tat <trieuanhtuan1408@gmail.com>
2024-04-01 19:25:42 +0700

    aspect config

---
+++ src/main/java/tat/com/eduhub/config/AspectConfig.java
@@ -0,0 +1,12 @@
+package tat.com.eduhub.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableAspectJAutoProxy
+@EnableTransactionManagement
+public class AspectConfig {
+
+}
