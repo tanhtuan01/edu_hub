034798f41db5dd231161082d88390b74b1a28aa9
tat <trieuanhtuan1408@gmail.com>
2024-04-02 00:23:54 +0700

    add utils

---
+++ src/main/java/tat/com/eduhub/Base/UserSchoolUtils.java
@@ -0,0 +1,23 @@
+package tat.com.eduhub.base;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.ui.Model;
+
+import tat.com.eduhub.entity.School;
+import tat.com.eduhub.entity.User;
+import tat.com.eduhub.service.SchoolService;
+import tat.com.eduhub.service.UserService;
+
+public class UserSchoolUtils {
+	
+
+	public static void populateUserAndSchool(UserService userService,SchoolService schoolService,String domain ,
+			Authentication authentication, Model model) {
+        User user = userService.findByEmail(authentication.getName());
+        School school = schoolService.findByDomain(domain.trim());
+
+        model.addAttribute("user", user);
+        model.addAttribute("school", school);
+    }
+	
+}
