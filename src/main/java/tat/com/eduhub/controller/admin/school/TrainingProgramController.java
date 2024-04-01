f9ef783abb8103940812ae4f26c2a538dd446430
tat <trieuanhtuan1408@gmail.com>
2024-04-02 00:25:25 +0700

    add component @SchoolAccountCheck

--- src/main/java/tat/com/eduhub/controller/admin/school/TrainingProgramController.java
+++ src/main/java/tat/com/eduhub/controller/admin/school/TrainingProgramController.java
@@ -13,7 +13,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import tat.com.eduhub.base.BASE_FIELD;
 import tat.com.eduhub.base.BASE_METHOD;
+import tat.com.eduhub.base.UserSchoolUtils;
+import tat.com.eduhub.component.SchoolAccountCheck;
 import tat.com.eduhub.component.UserHelper;
+import tat.com.eduhub.entity.School;
+import tat.com.eduhub.entity.User;
+import tat.com.eduhub.service.SchoolService;
+import tat.com.eduhub.service.UserService;
 
 @Controller
 @RequestMapping(value = "/s-admin/{domain}/training-program")
@@ -22,23 +28,38 @@ public class TrainingProgramController {
 	@Autowired
 	private UserHelper userHelper;
 	
+	@Autowired
+	private SchoolService schoolService;
+	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping(value = {"/create", ""})
-	public String createTrainingProgramPage(Model model, @PathVariable(name = "domain") String domain) {
+	@SchoolAccountCheck
+	public String createTrainingProgramPage(Model model, @PathVariable(name = "domain") String domain,
+			Authentication authentication) {
 		model.addAttribute("domain", domain);
+		
+		UserSchoolUtils.populateUserAndSchool(userService, schoolService, domain, authentication, model);
+		
 		BASE_METHOD.FragmentAdminSchool("create_training_program", model);
 		return BASE_FIELD.SCHOOL_ADMIN_LAYOUT;
 	}
 	
 	@PostMapping(value = "/create")
-	public String saveTrainingProgramName(Authentication authentication,@RequestParam(name = "name")String name, @ModelAttribute("domain")String domain) {
+	@SchoolAccountCheck
+	public String saveTrainingProgramName(Model model, Authentication authentication,@RequestParam(name = "name")String name, @ModelAttribute("domain")String domain) {
 		System.err.println(name);
 		System.err.println("domain: " + domain);
 		System.err.println(userHelper.getUseAuthenticated(authentication));
+		UserSchoolUtils.populateUserAndSchool(userService, schoolService, domain, authentication, model);
 		return "redirect:/s-admin/"+domain+"/training-program/write";
 	}
 	
 	@GetMapping(value = "/write")
-	public String writeTrainingProgramPage() {
-		return "redirect";
+	@SchoolAccountCheck
+	public String writeTrainingProgramPage(Model model) {
+		BASE_METHOD.FragmentAdminSchool("create_training_program", model);
+		return BASE_FIELD.SCHOOL_ADMIN_LAYOUT;
 	}
 }
