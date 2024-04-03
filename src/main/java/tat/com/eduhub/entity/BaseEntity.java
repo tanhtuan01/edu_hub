3a519662e4a6b5b78acf01ea8c0d8ed7bee63084
tat <trieuanhtuan1408@gmail.com>
2024-04-04 02:46:13 +0700

    update get set

--- src/main/java/tat/com/eduhub/entity/BaseEntity.java
+++ src/main/java/tat/com/eduhub/entity/BaseEntity.java
@@ -76,6 +76,22 @@ public class BaseEntity {
 		return formatDate;
 	}
 
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getModifiedBy() {
+		return modifiedBy;
+	}
+
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+
 	
 	
 }
