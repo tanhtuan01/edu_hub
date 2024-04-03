72e8ccfbfb16ac72f5d446ab9f3cbc0ed17bcf43
tat <trieuanhtuan1408@gmail.com>
2024-04-04 02:48:46 +0700

    update repo

--- src/main/java/tat/com/eduhub/repository/ModuleRepository.java
+++ src/main/java/tat/com/eduhub/repository/ModuleRepository.java
@@ -1,8 +1,11 @@
 package tat.com.eduhub.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import tat.com.eduhub.entity.Modules;
 import tat.com.eduhub.entity.School;
@@ -11,5 +14,7 @@ import tat.com.eduhub.entity.School;
 public interface ModuleRepository extends JpaRepository<Modules, Long>{
 
 	Page<Modules> findBySchool(School school, Pageable pageable);
+	
+	List<Modules> findBySchool(School school);
 	
 }
