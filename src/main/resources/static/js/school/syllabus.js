9165e884db0484cd84297bea8c9911d1cc0e70e1
tat <trieuanhtuan1408@gmail.com>
2024-04-04 02:52:49 +0700

    add js

---
+++ src/main/resources/static/js/school/syllabus.js
@@ -0,0 +1,44 @@
+function checkFileExtension(input) {
+  const allowedExtensions = ['doc', 'docx', 'ppt', 'pptx', 'txt', 'pdf'];
+  const fileExtension = input.value.split('.').pop().toLowerCase();
+  
+  if (!allowedExtensions.includes(fileExtension)) {
+    input.value = ''; 
+    alert('Chỉ hỗ trợ các file: Word, Powerpoint, Text, Pdf');
+  }
+}
+
+
+function beforeSubmitSearchForm(e){
+	event.preventDefault()
+	
+	var idModule = e.querySelector("#idModule").value
+	var keyword = e.querySelector("input[name='keyword']").value
+	
+	if(idModule == 0 && keyword.trim().length > 0){
+		e.submit()
+	}
+	if(idModule != 0 && keyword.trim().length == 0){
+		e.submit()
+	}
+	if(idModule != 0 && keyword.trim().length != 0){
+		e.submit()
+	}
+		
+}
+
+function previewSyllabus(e){
+	/*event.preventDefault()
+	
+	console.log(e)
+
+	var baseUrl = window.location.origin;
+
+	var hrefFile = e.getAttribute('href')
+	
+	var urlFile = baseUrl + hrefFile
+	
+	var viewerUrl = 'https://docs.google.com/viewer?url=' + urlFile;
+	
+	window.open(viewerUrl, '_blank');*/
+}
