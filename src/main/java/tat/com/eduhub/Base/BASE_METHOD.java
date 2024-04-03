31b2049a9e66401a85aaf9b4a6724d05301f9f77
tat <trieuanhtuan1408@gmail.com>
2024-04-04 02:42:38 +0700

    update method

--- src/main/java/tat/com/eduhub/Base/BASE_METHOD.java
+++ src/main/java/tat/com/eduhub/Base/BASE_METHOD.java
@@ -1,14 +1,24 @@
 package tat.com.eduhub.base;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.SecureRandom;
+import java.text.Normalizer;
+import java.time.LocalDateTime;
 import java.util.Random;
 
 import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartFile;
 
 public class BASE_METHOD {
 
 	private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     private static final Random RANDOM = new SecureRandom();
+    private static LocalDateTime localDateTime = LocalDateTime.now();
 	
 	public static void FragmentWeb(String fragment, Model model) {
 		String fragmentUrl =  "fragment/web/" + fragment;
@@ -36,4 +46,48 @@ public class BASE_METHOD {
 
         return sb.toString();
 	}
+	
+	public static String createSyllabusFileName() {
+		return "."+randomString(4) +"." + localDateTime.getDayOfMonth() +"" + localDateTime.getMonthValue() + localDateTime.getYear();
+	}
+	
+	public static String syllabusLecturerPathUpload(String domain, String fileName) {
+		Path path = Paths.get("src", "main", "resources", "static", "file", "syllabus", "lecturer", domain);
+		if(!Files.exists(path)) {
+			try {
+				Files.createDirectories(path);
+				System.err.println("Create path: " + domain + " => " + path);
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.err.println("Không thể tạo path: " + e.getMessage());
+			}
+		}
+		String filePath = Paths.get("src", "main", "resources", "static", "file", "syllabus", "lecturer", domain, fileName)
+				.toString();
+		return filePath;
+	}
+	
+	public static String removeAccents(String input) {
+		String normalized = Normalizer.normalize(input, Normalizer.Form.NFD);
+        String unaccented = normalized.replaceAll("\\p{InCombiningDiacriticalMarks}+", "").toLowerCase();
+        unaccented = unaccented.replaceAll("đ", "d");
+        unaccented = unaccented.replaceAll(" ","_");
+        return unaccented;
+	}
+	
+	public static String getExtensionFileName(MultipartFile file) {
+	    String filename = file.getOriginalFilename();
+	    String extension = "";
+
+	    if (filename != null && !filename.isEmpty()) {
+	        int dotIndex = filename.lastIndexOf('.');
+	        if (dotIndex > 0 && dotIndex < filename.length() - 1) {
+	            extension = filename.substring(dotIndex + 1);
+	        }
+	    }
+
+	    return extension;
+	}
+	
+
 }
