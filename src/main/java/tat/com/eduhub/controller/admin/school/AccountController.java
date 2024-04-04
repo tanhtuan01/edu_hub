1e80d0e7ea2c0baa34b32c8fbc692b05c8d43928
tat <trieuanhtuan1408@gmail.com>
2024-04-05 01:33:46 +0700

    add controller

---
+++ src/main/java/tat/com/eduhub/controller/admin/school/AccountController.java
@@ -0,0 +1,10 @@
+package tat.com.eduhub.controller.admin.school;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/school-admin/{domain}/quan-ly-tai-khoan/giang-vien")
+public class AccountController {
+
+}
