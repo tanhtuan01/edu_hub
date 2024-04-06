f3d3c3dcc2f60976b89d8aeeb6e012db0604b882
tat <trieuanhtuan1408@gmail.com>
2024-04-07 01:34:22 +0700

    add method

--- src/main/java/tat/com/eduhub/repository/TeacherOfSchoolRepository.java
+++ src/main/java/tat/com/eduhub/repository/TeacherOfSchoolRepository.java
@@ -1,5 +1,7 @@
 package tat.com.eduhub.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import tat.com.eduhub.entity.School;
@@ -10,4 +12,5 @@ public interface TeacherOfSchoolRepository extends JpaRepository<TeacherOfSchool
 
 	boolean existsByUserAndSchoolAndIsAdminTrue(User user, School school);
 	
+	Page<TeacherOfSchool> findBySchoolAndIsAdminFalse(School school, Pageable pageable);
 }
