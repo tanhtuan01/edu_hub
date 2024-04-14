5a03ea1c5dae25af88415dc3d5f5e6698a0ae1f3
tat <trieuanhtuan1408@gmail.com>
2024-04-14 23:12:36 +0700

    update repo

--- src/main/java/tat/com/eduhub/repository/TrainingProgramRepository.java
+++ src/main/java/tat/com/eduhub/repository/TrainingProgramRepository.java
@@ -1,9 +1,15 @@
 package tat.com.eduhub.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import tat.com.eduhub.entity.TrainingProgram;
+import tat.com.eduhub.entity.School;
+
 
 public interface TrainingProgramRepository extends JpaRepository<TrainingProgram, Long>{
 
+	List<TrainingProgram> findBySchool(School school);
+	
 }
