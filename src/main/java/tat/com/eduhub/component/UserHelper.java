e844481fc73cf0b9275647a6f1bc495cbc2cd600
tat <trieuanhtuan1408@gmail.com>
2024-04-20 14:20:16 +0700

    store and get user data info with session

--- src/main/java/tat/com/eduhub/component/UserHelper.java
+++ src/main/java/tat/com/eduhub/component/UserHelper.java
@@ -1,16 +1,25 @@
 package tat.com.eduhub.component;
 
-import org.springframework.security.core.Authentication;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+
+import tat.com.eduhub.dto.UserDataInfo;
 
 @Component
 public class UserHelper {
-
-	public String getUseAuthenticated(Authentication authentication) {
-		if(authentication != null) {
-			return authentication.getName();
-		}
-		return "";
-	}
 	
+	public void storeUserDataInfo(UserDataInfo userDataInfo, HttpServletRequest request) {
+	    HttpSession session = request.getSession();
+	    session.setAttribute("userDataInfo", userDataInfo);
+	}
+
+	public UserDataInfo getUserDataInfo(HttpServletRequest request, Model model) {
+	    HttpSession session = request.getSession();
+	    UserDataInfo userDataInfo = (UserDataInfo) session.getAttribute("userDataInfo");
+	    model.addAttribute("user", userDataInfo);
+	    return userDataInfo;
+	}
 }
