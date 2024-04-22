e5667f821bf6233c8e5bca4a301626ea9ec84cf9
tat <trieuanhtuan1408@gmail.com>
2024-04-22 08:16:14 +0700

    save user after google login

--- src/main/java/tat/com/eduhub/controller/GoogleLogin.java
+++ src/main/java/tat/com/eduhub/controller/GoogleLogin.java
@@ -1,21 +1,50 @@
 package tat.com.eduhub.controller;
 
 import java.security.Principal;
+import java.util.Arrays;
+import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import tat.com.eduhub.base.BASE_METHOD;
+import tat.com.eduhub.component.UserHelper;
+import tat.com.eduhub.dto.UserDataInfo;
+import tat.com.eduhub.entity.Role;
+import tat.com.eduhub.entity.User;
+import tat.com.eduhub.service.UserService;
 
 @Controller
 public class GoogleLogin {
+	
+	@Autowired
+	private UserHelper userHelper;
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	                                                                                                                                                                                     
 
 	@GetMapping(value = "/afterGoogleLoginSuccess")
-	public String googleLoginSuccess(Principal principal, Model model) {
+	public String googleLoginSuccess(Principal principal, Model model,
+			HttpServletRequest request, HttpServletResponse response) {
 		if (principal instanceof OAuth2AuthenticationToken) {
 	        OAuth2AuthenticationToken authenticationToken = (OAuth2AuthenticationToken) principal;
-	        
+	       
 	        boolean authenticated = authenticationToken.isAuthenticated();
 	        System.err.println(authenticated);
 	        
@@ -27,8 +56,34 @@ public class GoogleLogin {
 	        System.err.println("Name: " + name);
 	        model.addAttribute("name", name);
 	        model.addAttribute("email", email);
+	        Optional<String> role = oauth2User.getAuthorities().stream().map(GrantedAuthority::getAuthority).findFirst();
+	       
+	        User findUser = userService.findByEmail(email);
+	        System.err.println(principal.getName());
+	        if(findUser == null) {
+	        	User user = new User();
+	 	        user.setEmail(email);
+	 	        user.setReceiveMail(email);
+	 	        user.setUserName(name);
+	 	        user.setAvt("no-avatar.png");
+	 	        user.setRoles(Arrays.asList(new Role(role.get())));
+	 	        user.setType("google_account");
+	 	        user.setPasswords(passwordEncoder.encode(principal.getName()));
+	 	        userService.create(user);
+	        }else {
+	        	System.err.println("Have System Account");
+	        }
+	        
+//	        SavedRequest savedRequest = cache.getRequest(request, response);
+//
+//		    String redirectUrl = savedRequest != null ? savedRequest.getRedirectUrl() : "/";
+//		    
+//		    System.err.println(redirectUrl);
 	    }
 		return "googleLoginSuccess";
 	}
+
+	
+	
 	
 }
