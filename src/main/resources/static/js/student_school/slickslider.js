cfb7bbc789941267ec3c12a6f0a32893f29c98c5
tat <trieuanhtuan1408@gmail.com>
2024-04-27 14:22:38 +0700

    add static files

---
+++ src/main/resources/static/js/student_school/slickslider.js
@@ -0,0 +1,32 @@
+
+createSlickSlider()
+
+function createSlickSlider() {
+        if(!document.querySelector(".list-tp")){
+            return
+        }
+ 	const list = document.querySelectorAll(".list-tp .tp-item").length
+ 	var slidesToShow = 4;
+ 	var slidesPerRow = 4;
+ 	var rows = parseInt((list - 1) / slidesPerRow)
+ 	
+ 	if(rows > 1){
+		 slidesPerRow = 1;
+	 }
+ 	console.log(slidesToShow)
+ 	console.log(slidesPerRow)
+ 	console.log(rows)
+ 	
+    $(document).ready(function() {
+        $(".list-tp").slick({
+            slidesToShow: 4,
+            rows: `${rows}`,
+            slidesPerRow: 1,
+            lazyLoad: 'ondemand',
+            dots: true,
+           prevArrow: '<button class="btn-slick-prev pointer"><i class="fa-solid fa-circle-chevron-left"></i></button>',
+           nextArrow: '<button class="btn-slick-next pointer"><i class="fa-solid fa-circle-chevron-right"></i></button>'
+
+        });
+    });
+}
