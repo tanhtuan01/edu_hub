bf1d9a3aac81aa8b5f490bcb80450b1c6c5a3aaf
tat <trieuanhtuan1408@gmail.com>
2024-04-28 12:54:38 +0700

    update manage account lecturer

--- src/main/java/tat/com/eduhub/controller/lecturer/school/LecturerAccount.java
+++ src/main/java/tat/com/eduhub/controller/lecturer/school/LecturerAccount.java
@@ -1,12 +1,19 @@
 package tat.com.eduhub.controller.lecturer.school;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import tat.com.eduhub.base.BASE_FIELD;
 import tat.com.eduhub.base.BASE_METHOD;
@@ -36,6 +43,28 @@ public class LecturerAccount {
 		model.addAttribute("user", user);
 		
 		return BASE_FIELD.LECTURER_SCHOOL_LAYOUT;
+	}
+	
+	@PostMapping(value = "/cap-nhat")
+	public String updateLecturerAccount(@PathVariable(name = "domain") String domain,
+			@ModelAttribute(name = "user") User user, @RequestParam(name = "newAvt") MultipartFile newAvt) {
+		User u = userService.findByEmail(user.getEmail());
+		if(newAvt.getOriginalFilename().length() > 0) {
+			String oldImage = u.getAvt();
+			String fileName = BASE_METHOD.randomString(18) + BASE_METHOD.createStrDateNow() +"." + BASE_METHOD.getExtensionFileName(newAvt); 
+			u.setAvt(fileName);
+			try {
+				Files.write(Paths.get(BASE_METHOD.lecturerImagePathUpload(fileName)), newAvt.getBytes());
+				if(!oldImage.equals("default_lecturer_avt.png")) {
+					Files.delete(Paths.get(BASE_METHOD.lecturerImagePathUpload(oldImage)));
+				}
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+		u.setReceiveMail(user.getReceiveMail());
+		userService.saveAndGetId(u);
+		return "redirect:/school-lecturer/" + domain + "/quan-ly-tai-khoan?updated";
 	}
 	
 }
