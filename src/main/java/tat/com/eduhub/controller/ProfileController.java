09b2ba29a062c6e4b7d3cbc89ca6c76123824aa1
tat <trieuanhtuan1408@gmail.com>
2024-05-05 13:50:40 +0700

    redirect to profile

--- src/main/java/tat/com/eduhub/controller/ProfileController.java
+++ src/main/java/tat/com/eduhub/controller/ProfileController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import net.bytebuddy.asm.Advice.Return;
 import tat.com.eduhub.component.UserHelper;
 import tat.com.eduhub.dto.UserDataInfo;
 
@@ -23,11 +24,16 @@ public class ProfileController {
 		UserDataInfo userDataInfo = userHelper.getUserDataInfo(request, model);
 		switch (userDataInfo.getRole()) {
 		case "ROLE_LECTURER":
-			return "redirect:/lecturer/khoa-hoc";
-		case "ROLE_ADMIN_SCHOOOL":
+			return "redirect:/lecturer/trang-chu";
+		case "ROLE_ADMINSCHOOL":
 			return "redirect:/school-admin/" + userDataInfo.getDomain();
 		case "ROLE_LECTURERSCHOOL":
 			return "redirect:/school-lecturer/" +userDataInfo.getDomain();
+		case "ROLE_STUDENT":
+		case "ROLE_USER":
+			return "redirect:/e-student/tai-khoan-cua-toi";
+		case "ROLE_SUPERADMIN":
+			return "redirect:/eh-admin/trang-chu";
 		default:
 			break;
 		}
